dc493b24da026849a3bff789aafabd1585eba54a
greenmoon55 <greenmoon55@gmail.com>
2014-06-03 23:43:10 +0800

    basic lister app is ready

---
+++ lister/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from lister.models import Version
+
+class IndexView(generic.ListView):
+    template_name = 'lister/index.html'
+    context_object_name = 'version_list'
+
+    def get_queryset(self):
+        return Version.objects.order_by('-id')
