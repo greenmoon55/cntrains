20eb8f2ee03d0e19176d0059674af4097a359ffa
greenmoon55 <greenmoon55@gmail.com>
2014-06-05 00:15:30 +0800

    simple file download module

--- lister/cron.py
+++ lister/cron.py
@@ -3,12 +3,21 @@
 import kronos
 import random
 import requests
+import re
 
+# from lxml import html
 from lister.models import Version
 
 @kronos.register('0 0 * * *')
 def check_update():
     r = requests.get('http://www.smskb.com/soft/html/12.html')
+    page = r.text
+    match = re.search("http://www.smskb.com/down/\S+rar", page)
+    addr = match.group(0)
+    filename = re.search("down/(\S+rar)", addr).group(1)
+    r = requests.get(addr)
+    with open(filename, 'wb') as fd:
+        fd.write(r.content)
 
     complaints = [
         "I forgot to migrate our applications's cron jobs to our new server! Darn!",
