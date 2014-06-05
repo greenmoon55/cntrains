2eb2e77098420d9496f524e887a4a77c66f664cf
greenmoon55 <greenmoon55@gmail.com>
2014-06-05 22:21:24 +0800

    save to database

--- lister/cron.py
+++ lister/cron.py
@@ -1,11 +1,14 @@
 # app/cron.py
 
+import hashlib
 import kronos
 import random
 import requests
 import re
+import datetime
 
-# from lxml import html
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils import timezone
 from lister.models import Version
 
 @kronos.register('0 0 * * *')
@@ -15,9 +18,19 @@ def check_update():
     match = re.search("http://www.smskb.com/down/\S+rar", page)
     addr = match.group(0)
     filename = re.search("down/(\S+rar)", addr).group(1)
+    date_str = re.search("\d+", filename).group(0)
+    date = datetime.datetime.strptime(date_str, '%Y%m%d')
+
     r = requests.get(addr)
-    with open(filename, 'wb') as fd:
-        fd.write(r.content)
+    try:
+        record = Version.objects.get(release_date=date)
+    except ObjectDoesNotExist:
+        with open(filename, 'wb') as fd:
+            fd.write(r.content)
+        m = hashlib.md5()
+        m.update(r.content)
+        Version.objects.create(release_date=date, update_date=timezone.now(), link=filename, md5_hash=m.hexdigest())
+
 
     complaints = [
         "I forgot to migrate our applications's cron jobs to our new server! Darn!",
