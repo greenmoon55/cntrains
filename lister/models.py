c2474d35dff8af7046fa9f61c7ee2334900cecd4
greenmoon55 <greenmoon55@gmail.com>
2014-11-14 22:14:49 +0800

    code cleanup

--- lister/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Version(models.Model):
-    link = models.CharField(max_length=255)
-    release_date = models.DateField()
-    update_date = models.DateField()
-    md5_hash = models.CharField(max_length=32)
-    
