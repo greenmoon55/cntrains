8af885098ee391ba813597fa69a1be879fe25023
greenmoon55 <greenmoon55@gmail.com>
2014-11-16 15:11:57 +0800

    update reload.sh

--- reload/reload.sh
+++ reload/reload.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo docker pull greenmoon55/private:latest
-docker stop cntrains
-docker rm cntrains
-docker run --dns 8.8.8.8 --name cntrains -d -p 80:80 greenmoon55/private:latest
+sudo docker pull greenmoon56/private:latest
+#sudo docker rm -f cntrains-redis
+sudo docker rm -f cntrains
+#sudo docker run --dns 8.8.8.8 --name cntrains-redis -d redis:2.8.17
+sudo docker run --dns 8.8.8.8 --name cntrains -d -p 80:80 --dns 8.8.8.8 -d --link cntrains-redis:redis
